99b3d45118e0c1f66ce87ea77303b9d7f3a3cdf5
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2022-08-29 22:18:47 -0300

    feat: renderiza a lista de paises

---
+++ src/js/Request.js
@@ -0,0 +1,52 @@
+import { criaCard } from "./criaCardLista.js"
+
+class RequestApi{
+
+    static url     = "https://kenzie-olympics.herokuapp.com/paises"
+    static headers = {"Content-Type": "application/json"}
+
+    static async PegaInformacao(){
+
+        const infoPaises = await fetch(`${this.url}`,{
+
+            method: "GET",
+            headers: this.headers
+
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+
+            const tabela = document.querySelector(".listaInformacoes")
+
+            resp.forEach(element => {
+
+                let {id,country, flag_url, medal_bronze, medal_gold, medal_silver} = element
+
+                let totalMedal = medal_bronze + medal_gold + medal_silver
+
+                const paisesFormatado = {
+
+                    pais: country,
+                    bandeira: flag_url,
+                    ouro: medal_gold,
+                    prata: medal_silver,
+                    bronze: medal_bronze,
+                    totalMedalhas: totalMedal
+
+                }
+
+                tabela.appendChild(criaCard(id, paisesFormatado))
+
+            })
+
+        })
+        .catch(err => console.log(err))
+
+        return infoPaises
+
+    }
+    
+
+}
+
+RequestApi.PegaInformacao()
