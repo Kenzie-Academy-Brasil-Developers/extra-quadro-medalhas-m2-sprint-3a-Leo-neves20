4a513a47607f0d82e4d1c5e819e65c5a8c0c7d9f
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2022-08-30 13:08:08 -0300

    feat: cria a funcionalidade de fazer pesquisa

---
+++ src/js/FazPesquisaTabela.js
@@ -0,0 +1,85 @@
+import { criaCard } from "./criaCardLista.js"
+import { RequestApi } from "./Request.js"
+
+const formPesquisa = document.querySelector("form")
+const tabela       = document.querySelector(".listaInformacoes")
+const inputValue   = document.querySelector("input")
+const mensagemErro = document.querySelector(".mensagemErro")
+
+
+
+const filtroPesquisa = async () => {
+
+    formPesquisa.addEventListener("click", async (event) => {
+
+    
+        if(event.target.tagName == "BUTTON"){
+
+            tabela.innerHTML = ""
+
+            const api = await fetch(`${RequestApi.url}`, {
+
+                method: "GET",
+                headers: RequestApi.headers
+
+            })
+            .then(resp => resp.json())
+            .then(resp => {
+
+                resp.forEach(element => {
+
+                    let nomePaisFormatado     = element.country.toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+                    let nomePesquisaFormatado = inputValue.value.toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
+                    let {id,country, flag_url, medal_bronze, medal_gold, medal_silver} = element
+
+                        let totalMedal        = medal_bronze + medal_gold + medal_silver
+                        const paisesFormatado = {
+
+                            pais: country,
+                            bandeira: flag_url,
+                            ouro: medal_gold,
+                            prata: medal_silver,
+                            bronze: medal_bronze,
+                            totalMedalhas: totalMedal
+
+                        }
+
+                    if(nomePaisFormatado == nomePesquisaFormatado){
+
+                        mensagemErro.classList.add("hidden")
+
+                        tabela.appendChild(criaCard(id, paisesFormatado))
+
+                        mensagemErro.innerText = ""
+                       
+                    }else if(nomePesquisaFormatado === "todos"){
+
+                        mensagemErro.innerText = ""
+
+                        tabela.appendChild(criaCard(id, paisesFormatado))
+
+                    }else{
+
+                        mensagemErro.innerText = "nome inavlido"
+
+                        tabela.appendChild(criaCard(id, paisesFormatado))
+
+                    }
+
+                })
+
+            })
+            .catch(err => console.log(err))
+            return api
+
+        }    
+            
+    })
+
+}
+
+filtroPesquisa()
+
+
+
